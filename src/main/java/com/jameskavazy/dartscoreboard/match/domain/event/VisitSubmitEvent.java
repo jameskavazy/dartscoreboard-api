936cf2bae84503363de6b671438d8c74d9bde44f
JamesK <james.kavazy@gmail.com>
2026-01-23 16:26:44 +0000

    fix: Remove component anno from VisitSubmitEvent message class, as should not be bean

--- src/main/java/com/jameskavazy/dartscoreboard/match/domain/event/VisitSubmitEvent.java
+++ src/main/java/com/jameskavazy/dartscoreboard/match/domain/event/VisitSubmitEvent.java
@@ -4,7 +4,6 @@ import com.jameskavazy.dartscoreboard.match.domain.model.entity.Visit;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.stereotype.Component;
 
-@Component
 public class VisitSubmitEvent extends ApplicationEvent {
 
 
